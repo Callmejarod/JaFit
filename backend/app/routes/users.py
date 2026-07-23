94f52294955a6a242b5df0775e17e09f1490ba21
Jarod Atienzo <Jarodmendozaatienzo@gmail.com>
2026-07-22 20:58:48 -0700

    Add user registration with MongoDB and password hashing

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
-from models.user import User
-from database.mongodb import users_collection
+from fastapi import APIRouter, HTTPException
+from app.models.user import User
+from app.database.mongodb import users_collection
+from app.services.security import hash_password
+from datetime import datetime
 
 
 router = APIRouter(
@@ -12,11 +14,31 @@ router = APIRouter(
 @router.post("/register")
 def register_user(user: User):
 
-    user_data = user.dict()
+    # Check if email already exsist
+    existing_user = users_collection.find_one({"email": user.email})
+
+    if existing_user:
+        raise HTTPException(
+            status_code = 400,
+            detail="Email already registered"
+        )
+
+    print(user.password)
+    print(len(user.password))
+
+    hashed_password = hash_password(user.password)
+
+
+
+    user_data = {
+        "username": user.username,
+        "email": user.email,
+        "password_hash": hashed_password,
+        "created_at": datetime.now()
+    }
 
     users_collection.insert_one(user_data)
 
     return {
         "message": "User created successfully",
-        "user": user_data
     }
