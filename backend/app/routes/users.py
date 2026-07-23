9d7fe451f08fc2959c2cfed93828cbba6c99a0aa
Jarod Atienzo <Jarodmendozaatienzo@gmail.com>
2026-07-23 15:56:53 -0700

    Add JWT authenication and login endpoint

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, HTTPException
 from app.models.user import User
+from app.models.login import LoginRequest
 from app.database.mongodb import users_collection
-from app.services.security import hash_password
+from app.services.security import hash_password, verify_password
+from app.services.auth import create_access_token
 from datetime import datetime, timezone
 
 
@@ -38,4 +40,41 @@ def register_user(user: User):
 
     return {
         "message": "User created successfully",
+    }
+
+@router.post("/login")
+def login(login_data: LoginRequest):
+
+    # Find user by email
+    user = users_collection.find_one(
+        {"email": login_data.email}
+    )
+
+    # User not found
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    # Verify password
+    if not verify_password(
+        login_data.password,
+        user["password_hash"]
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    # Create JWT
+    access_token = create_access_token(
+        {
+            "sub": user["email"]
+        }
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
     }
